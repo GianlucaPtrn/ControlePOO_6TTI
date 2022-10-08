7b97e5ce76b25a12f846a79585be950ca9e225a3
GianlucaPtrn <91669728+GianlucaPtrn@users.noreply.github.com>
2022-10-08 10:14:49 +0200

    Add files via upload

---
+++ I1P622_GianlucaPetronio/I1P622_GianlucaPetronio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+
+namespace I1P622_GianlucaPetronio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Feu de signalisation");
+            Console.WriteLine();
+
+            Signalisation[] signalisation = new Signalisation[2];
+
+            for (int i = 0; i < signalisation.Length; i++)
+            {
+                Console.WriteLine("état des feux :");
+                Console.WriteLine("---------------");
+                Console.WriteLine(signalisation.afficheColorAndType());
+
+                Console.WriteLine("changement d'état :");
+                Console.WriteLine("-------------------");
+
+                Console.WriteLine("feu clignotant :");
+                Console.WriteLine("----------------");
+            }
+        }
+    }
+}
---
+++ I1P622_GianlucaPetronio/I1P622_GianlucaPetronio/feuDeSignalisation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I1P622_GianlucaPetronio
+{
+    class Signalisation
+    {
+        //variable 
+        private string _Signalisation1001;
+        private string _Signalisation007;
+
+        //constructeur
+        public Signalisation (string _Signalisation1001, string _Signalisation007)
+        {
+            this._Signalisation1001 = _Signalisation1001;
+            this._Signalisation007 = _Signalisation007;
+        }
+
+        //methode
+        public int changeColor(string _Signalisation1001, string _Signalisation007)
+        {
+            int color = 0;
+            for(int i = 0; i < 3; i++)
+            {
+                
+                if(i == 0)
+                {
+                    return color = 1;
+                }
+                else if (i == 1)
+                {
+                    return color = 2;
+                }
+                else
+                {
+                    return color = 3;
+                }
+            }
+
+            return color;
+        }
+
+        public int clignoter(string _Signalisation1001, string _Signalisation007)
+        {
+            return ...;
+        }
+
+        public string afficheColorAndType(int color, string _Signalisation1001, string _Signalisation007)
+        {
+            _Signalisation007 = "feu de clignotement 007";
+            _Signalisation1001 = "feu de clignotement 1001";
+            
+            string phrase = _Signalisation007 + color;
+            string phrase2 = _Signalisation1001 + color;
+
+            return phrase;
+        }
+    }
+}
